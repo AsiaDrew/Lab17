87e3593f43d2bb5b511e43ce82a2cafd58d1f003
AsiaDrew <asiadrew2@outlook.com>
2022-07-22 18:18:16 -0400

    first

---
+++ CoffeShopProductList/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using CoffeShopProductList.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeShopProductList.Controllers
+{
+    public class ProductController : Controller
+    {
+        public CoffeeProductDbContext context = new CoffeeProductDbContext();
+        public IActionResult Index()
+        {
+            return View(context.Products.ToList());
+        }
+        public IActionResult Details(int productId)
+        {
+            Product p = context.Products.FirstOrDefault(p => p.Id == productId);
+            return View(p);
+        }
+        public IActionResult Products()
+        {
+            List<Product> products = context.Products.ToList();
+            return View(products);
+        }
+
+    }
+}
---
+++ CoffeShopProductList/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoffeShopProductList.Models
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double? Price { get; set; }
+        public string? Catergory { get; set; }
+    }
+}
